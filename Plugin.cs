fb637479e3cf83c71dc4aef9f3f5007e6de33e47
un-simp <uncraftablecreeper@gmail.com>
2024-04-05 17:35:50 +0100

    innit

---
+++ Plugin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using MonoMod;
+using On;
+using UnityEngine;
+using Zorro.Recorder;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using FfmpegEncoder = On.Zorro.Recorder.FfmpegEncoder;
+
+
+namespace CWLiveLeak
+{
+	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	public class Plugin : BaseUnityPlugin
+	{
+
+		private ConfigEntry<string> Icon;
+		public string pluginPath;
+		static internal ManualLogSource? LiveleakLogger;
+
+		private void Awake()
+		{
+			LiveleakLogger = base.Logger;
+			Icon = Config.Bind("General",      // The section under which the option is shown
+				"Icon",  // The key of the configuration option in the configuration file
+				"liveleak", // The default value
+				"What Icon is shown, available choices are: liveleak, hypercam, spookycam "); // Description of the option to show in the config file
+			LiveleakLogger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+			Init();
+			// looks like i dont need to harmony :yay:
+			On.Zorro.Recorder.FfmpegEncoder.RunFfmpeg += FfmpegEncoderOnRunFfmpeg;
+		}
+
+		private void Init()
+		{
+			Assembly callingAssembly = Assembly.GetCallingAssembly();
+			pluginPath = Path.GetDirectoryName(callingAssembly.Location);
+		}
+		
+		private IEnumerator FfmpegEncoderOnRunFfmpeg(FfmpegEncoder.orig_RunFfmpeg orig, Zorro.Recorder.FfmpegEncoder self, string arguments, bool displaywindow)
+		{
+			LiveleakLogger.LogInfo($"Adding {Icon.Value} In");
+			switch (Icon.Value)
+			{
+				// special case for liveleak as i think it looks nicer like this
+				case "liveleak":
+					arguments = arguments + $" -i {pluginPath}\\{Icon.Value.ToLower()}.png -filter_complex \"[0:v][3:v] overlay=25:25:enable='between(t,0,20)'\"";
+					break;
+				default:
+					arguments = arguments + $" -i {pluginPath}\\{Icon.Value.ToLower()}.png -filter_complex \"[0:v][3:v] overlay=0:0:enable='between(t,0,20)'\"";
+					break;
+			}
+			
+			
+			return orig(self,arguments,displaywindow);
+		}
+
+
+	}
+}
---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CWLiveLeak
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "CWLiveLeak";
+        public const string PLUGIN_NAME = "CWLiveLeak";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
